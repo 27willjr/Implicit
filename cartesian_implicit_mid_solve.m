7cf1e6f1152b6bf00ae1273dc45961cc3487e767
yanlif123 <124690499+yanlif123@users.noreply.github.com>
2024-02-05 21:21:53 +0000

    Add files via upload

--- cartesian_implicit_mid_solve.m
+++ cartesian_implicit_mid_solve.m
@@ -1,9 +1,11 @@
-N = [100, 250, 500, 1000];
-T = [5, 10, 25, 100, 150, 200];
+N = [10000];
+T = [5, 10, 15,25, 50, 100];
+L = 2;
+w = 8*pi;
 for j = 1:length(T)
 figure;
     for i = 1:length(N)
-        [t, y] = implicit_midpoint([0 T(j)], [pi/4, 0, pi/4, 0], N(i), @pendulum_cartesian_odes);
+        [t, y] = implicit_midpoint([0 T(j)], [0, -L*w^2, L*w, 0], N(i), @pendulum_cartesian_odes);
         plot(y(:, 1), y(:, 3), 'b');
         xlabel('x')
         ylabel('y')
@@ -11,8 +13,8 @@ figure;
     end
 
 hold off
-legend('N=100','N=250', 'N=500', 'N=1000');
-title(['Nonlinear Pendulum Motion with Implicit Midpoint Method : Time Length = ', num2str(T(j)), 's']);
+
+title(['Nonlinear Pendulum Motion with Implicit Midpoint Method using Cartesian Coordinates : Time Length = ', num2str(T(j)), 's'], 'FontSize', 7');
 end
 
 %
